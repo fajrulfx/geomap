80cfa2943898367bd4bdba3b339cc17e0b52cd4f
fajrulfx <ffajrul55@gmail.com>
2023-09-09 23:02:39 +0700

    animation adjustment

--- map.js
+++ map.js
@@ -6,11 +6,14 @@ var map = new mapboxgl.Map({
     center: [118.0149, -2.5489], // Indonesia coordinates
     zoom: 4, // Zoom level for Indonesia
     attributionControl: false,
-    dragRotate: false,
-    pitchWithRotate: false
   });
   
   map.addControl(new mapboxgl.NavigationControl());
+  // disable map rotation using right click + drag
+map.dragRotate.disable();
+ 
+// disable map rotation using touch rotation gesture
+map.touchZoomRotate.disableRotation();
 
   map.on('load', function() {
     var layers = map.getStyle().layers;
@@ -23,33 +26,23 @@ var map = new mapboxgl.Map({
             map.setLayoutProperty(layers[i].id, 'text-field', [
                 'format',
                 ['get', 'name_en'],
-                { 'font-scale': isMobile ? 2.0 : 1.3 }, // if mobile, font-scale is 2.0, else 1.2
+                { 'font-scale': isMobile ? 2.2 : 1.3 }, // if mobile, font-scale is 2.0, else 1.2
             ]);
         }
     }
 });
 
-  map.on('click', function(e) {
+map.on('click', function (e) {
     navigator.clipboard.writeText(`!guess ${e.lngLat.lat.toFixed(5)} ${e.lngLat.lng.toFixed(5)}`);
+  
     let alertBox = document.createElement('div');
     alertBox.innerHTML = "<p>Koordinat telah di-copy.<br>Silahkan paste di Youtube livechat</p>";
-    
-    const size = map.getContainer().getBoundingClientRect();
-    const point = map.project(e.lngLat);
-  
-    if (point.y < size.height / 2) {
-      alertBox.style.top = `${point.y + 35}px`;
-      alertBox.classList.add('alert', 'alert-top', 'show');
-    } else {
-      alertBox.style.top = `${point.y - 90}px`;
-      alertBox.classList.add('alert', 'alert-bottom', 'show');
-    }
-    alertBox.style.left = `${point.x}px`;
+    alertBox.classList.add('alert', 'alert-top', 'show', 'slide-up');
   
     document.body.appendChild(alertBox);
   
     setTimeout(() => {
-      alertBox.classList.remove('show');
+      alertBox.classList.remove('show', 'slide-up');
       setTimeout(() => alertBox.remove(), 2000);
     }, 3000);
   });
