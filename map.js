14cfbe7349826c0dacb661d8a5dce00dc95e3de9
fajrulfx <ffajrul55@gmail.com>
2023-09-10 15:32:36 +0700

    add marker and updated text

--- map.js
+++ map.js
@@ -1,4 +1,11 @@
 var mymap = L.map('mapid').setView([-2.5489, 118.0149], 4);
+var markerIcon = L.icon({
+    iconUrl: 'marker.png',
+    iconSize: [40, 40], // adjust the size according to your marker icon image
+    iconAnchor: [20, 40] // adjust the anchor point according to your marker icon image
+  });
+  var marker;
+var marker;
 
 L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=en&key=AIzaSyD0U_Q9bCY4FvFayVUM2vxFrRzuChJkKtY', {
     minZoom: 2,
@@ -7,27 +14,18 @@ L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=en&key=AIzaSyD
 }).addTo(mymap);
 
 mymap.on('click', function(e) {
+    if (marker) {
+        mymap.removeLayer(marker);
+      }
     navigator.clipboard.writeText(`!guess ${e.latlng.lat.toFixed(5)} ${e.latlng.lng.toFixed(5)}`);
 
-    let alertBox = document.createElement('div');
-  alertBox.innerHTML = "<p>Koordinat telah di-copy.<br>Silahkan paste di Youtube livechat</p>";
-  
-  const size = mymap.getSize();
-  const point = mymap.latLngToContainerPoint(e.latlng);
-  
-  alertBox.style.top = `${e.containerPoint.y}px`;
-alertBox.style.left = `${e.containerPoint.x}px`;
-
-if (e.containerPoint.y < size.y / 2) {
-    alertBox.classList.add('alert', 'alert-top', 'show');
-} else {
-    alertBox.classList.add('alert', 'alert-top', 'show');
-}
-
-  document.body.appendChild(alertBox);
-
-  setTimeout(() => {
-    alertBox.classList.remove('show');
-    setTimeout(() => alertBox.remove(), 2000);
-  }, 3000);
+    marker = L.marker(e.latlng, { icon: markerIcon }).addTo(mymap);
+    
+    var popupOffset = L.point(0, -25); // Adjust the Y offset to position the popup above the marker
+  var popupText = `
+    Lat: ${e.latlng.lat.toFixed(5)} Long: ${e.latlng.lng.toFixed(5)}<br><br>
+    <span style="font-weight: bold;">Koordinat telah di-copy, silahkan<br>
+    paste di Youtube livechat</span>
+  `;
+  marker.bindPopup(popupText, { offset: popupOffset, className: 'custom-popup' }).openPopup();
 });
